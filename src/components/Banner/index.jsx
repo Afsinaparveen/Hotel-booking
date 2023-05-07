1256cddf2dcbb8e666e4f352a3ece34c8bf1e592
afsina.parveen <tpsinu01@gmail.com>
2023-05-07 09:26:11 +0530

    machine test

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Banner() {
     return (
         <div className="banner">
@@ -9,9 +10,68 @@ function Banner() {
                     <h1>Let us plan a perfect your International holiday</h1>
                     <button>Explore now</button>
                 </div>
-            </div>
-            <div className="booking_div">
-
+                <div className="booking_div">
+                    <div className="booking">
+                        <div>
+                            <div className="banner_main_div">
+                                <div className="booking_header">
+                                <div><FontAwesomeIcon icon={faUser} /></div>
+                                    <h4>check in</h4>
+                                    <div><FontAwesomeIcon icon={faUser} /></div>
+                                </div>
+                            </div>
+                            <div className="booking_bof">
+                                <h2>Date</h2>
+                                <h4>Date</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="booking">
+                        <div>
+                            <div className="banner_main_div">
+                                <div className="booking_header">
+                                <div><FontAwesomeIcon icon={faUser} /></div>
+                                    <h4>Check out</h4>
+                                    <div><FontAwesomeIcon icon={faUser} /></div>
+                                </div>
+                            </div>
+                            <div className="booking_bof">
+                                <h2>Date</h2>
+                                <h4>Date</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="booking">
+                        <div>
+                            <div className="banner_main_div">
+                                <div className="booking_header">
+                                <div><FontAwesomeIcon icon={faUser} /></div>
+                                    <h4>Adult</h4>
+                                    <div><FontAwesomeIcon icon={faUser} /></div>
+                                </div>
+                            </div>
+                            <div className="booking_bof">
+                                <h2>Date</h2>
+                                <h4>Date</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="booking">
+                        <div>
+                            <div className="banner_main_div">
+                                <div className="booking_header">
+                                <div><FontAwesomeIcon icon={faUser} /></div>
+                                    <h4>Child</h4>
+                                    <div><FontAwesomeIcon icon={faUser} /></div>
+                                </div>
+                            </div>
+                            <div className="booking_bof">
+                                <h2>Date</h2>
+                                <h4>Date</h4>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
             </div>
         </div>
     )
